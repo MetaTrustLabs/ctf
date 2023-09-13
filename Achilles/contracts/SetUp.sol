efe36945aae7d2f4774901f10d0923f50d50d6a7
BloodKnifeSimon <heaven235@gmail.com>
2023-09-13 11:37:46 +0800

    no one know the address

--- Achilles/contracts/SetUp.sol
+++ Achilles/contracts/SetUp.sol
@@ -11,17 +11,19 @@ contract SetUp {
     PancakePair public pair = new PancakePair();
     WETH public weth = new WETH();
     Achilles public achilles = new Achilles(address(pair), address(weth));
+    address public yourAddress;
 
     constructor() {
         pair.initialize(address(achilles), address(weth));
         achilles.transfer(address(pair), achilles.balanceOf(address(this)));
         weth.transfer(address(pair), weth.balanceOf(address(this)));
         pair.sync();
+		yourAddress = msg.sender;
     }
 
     function isSolved() public view returns(bool) {
         require(pair.unlocked()==1);
-        require(weth.balanceOf(msg.sender) >= 100 ether);
+        require(weth.balanceOf(yourAddress) >= 100 ether);
         return true;
     }
 }
